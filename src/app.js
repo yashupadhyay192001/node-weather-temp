26c4e5b0d2b1516f7c067514f348bcebe1370415
yash upadhyay <yashupadhyay2001@gmail.com>
2020-03-12 10:42:39 +0530

    second error correction

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const path = require('path')
 const hbs =require('hbs')
 
 const app = express()
-const port = process.emv.PORT || 3000
+const port = process.env.PORT || 3000
 
 
 //define paths for express config.
